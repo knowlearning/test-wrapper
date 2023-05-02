6d9bc4f9c39faa4f9f62b3288e5042d01f6fc930
Jason Ford <fordjason@gmail.com>
2023-05-02 13:43:35 -0700

    Attempt to fix composition api build variable renaming

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    minifyIdentifiers: false // required so that names for refs that should be persistent in vue composition api components don't get minified
+  },
   plugins: [
     vue(),
     basicSsl()
