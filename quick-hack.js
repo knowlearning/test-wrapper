bfbcddabba223fb1b121d9871e14094bb10cbf58
Jason Ford <fordjason@gmail.com>
2023-05-02 20:00:26 -0700

    Remove hack

--- quick-hack.js
+++ quick-hack.js
@@ -27,28 +27,27 @@ export default async function (module, props) {
       const origSetupFn = component.setup
       const isReactiveRef = r => r && r.__v_isRef
       component.setup = function setupProxy(a, b) { // need to specifically add this here, otherwise second argument not passed in arguments array (probably because of webpack optimization...)
-        let refs = origSetupFn.call(this, a, b)
-        function doIt() {
-          Object
-            .entries(state)
-            .filter(([k]) => isReactiveRef(refs[k]))
-            .forEach(([k, v]) => refs[k].value = v)
+        const refs = origSetupFn.call(this, a, b)
 
-          Object
-            .entries(refs)
-            .filter(([_,r]) => isReactiveRef(r) && isScopeSerializable(r.value))
-            .forEach(([key, ref]) => {
-              watchEffect(() => {
-                if (state[key] !== ref.value) {
-                  state[key] = ref.value
-                }
-              })
+        console.log(refs)
+
+        Object
+          .entries(state)
+          .filter(([k]) => isReactiveRef(refs[k]))
+          .forEach(([k, v]) => refs[k].value = v)
+
+        Object
+          .entries(refs)
+          .filter(([_,r]) => isReactiveRef(r) && isScopeSerializable(r.value))
+          .forEach(([key, ref]) => {
+            watchEffect(() => {
+              if (state[key] !== ref.value) {
+                state[key] = ref.value
+              }
             })
+          })
 
-          return refs
-        }
-        if (typeof refs === 'function') return doIt
-        else return doIt()
+        return refs
       }
     }
   }
